1e33c2240504825d03c72cc3ae98dc7a8bdf67bb
jkosz <jan.koszewski@livingsocial.com>
2014-01-26 17:33:26 -0500

    Initial commit, models and migrations for item, customer, merchant, and order, invoices controller

---
+++ app/controllers/invoice_controller.rb
@@ -0,0 +1,7 @@
+class InvoiceController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DataEngineering::Application.config.secret_key_base = 'a60d807d4fa73050a0d7eed4b2451c132a751a76bb9082e89263ff273d277cdcbb2b8454f3179d13f0f9dec778975fb6cda5650b6e079114cc2ec3e63ba7331f'
